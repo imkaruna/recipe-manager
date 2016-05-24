065e047910da627d2907249663817eb06e2d379b
imkaruna <imkaruna@gmail.com>
2016-05-24 19:22:13 -0400

    Added devise gem for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :recipes do
-    resources :ingredients
+    resources :ingredients, controller: :recipes
   end
 
+  resources :ingredients, controller: :ingredients
   root 'recipes#index'
 end
