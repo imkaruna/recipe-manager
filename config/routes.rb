cc81996b0320e10c911f9b833588d62376a36dca
Karuna <imkaruna@gmail.com>
2016-06-01 16:25:04 -0400

    Removed unnecessary route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :recipes do
     resources :ingredients
-    resources :ingredients, only: [:destroy], as: :remove, method: 'post'
   end
 
   resources :ingredients
