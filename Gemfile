ca5a90c675acd30c388f47dc45a44b24d407904d
Karuna <imkaruna@gmail.com>
2016-06-01 17:54:24 -0400

    Added Omniauth for Facebook

--- Gemfile
+++ Gemfile
@@ -22,9 +22,15 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-gem "paperclip"
-gem 'devise'
 
+gem "paperclip"
+# Adds attachment
+
+gem 'devise'
+# Gem for authentication
+
+gem 'omniauth-facebook'
+# Gem for omniauth facebook authentication
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524235119) do
+ActiveRecord::Schema.define(version: 20160601205158) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20160524235119) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
