1dd37dee8dc2b3959749118c8f8ae5681937ce67
Karuna <imkaruna@gmail.com>
2016-06-10 23:54:56 -0400

    Added JSON model object and method on the model object

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,6 +1,32 @@
+function Recipe(id, name, description, comments) {
+  this.name = name;
+  this.description = description;
+  this.comments = comments;
+
+  this.displayInfo = function() {
+    var commentLength = this.comments.length;
+    return this.comments.length;
+  }
+}
+
+function getRecipeInfo(id) {
+    $.get( "recipes/"+id+".json", function( data ) {
+      var this_recipe = new Recipe(id, data.name, data.description, data.comments)
+      var commentLength = this_recipe.displayInfo();
+      $(".commentsNum_"+id).text(commentLength + ' comment');
+    });
+    return false;
+}
+
+
 $( document ).ready(function() {
   console.log( "ready!" );
-
+  $(".recipe-card").on('click',function(ev) {
+    var id = $(this).attr('data-id');
+    getRecipeInfo(id);
+    $('#recipe-info-'+id).slideToggle('fast');
+    ev.stopPropagation();
+});
   $('#comment_form').submit(function() {
     var valuesToSubmit = $(this).serialize();
     $.ajax({
